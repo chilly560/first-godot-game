1746beb04328d6c5509abc70d617fd54c26b677f
Joseph Canale <josephpcanale77@gmail.com>
2025-09-28 23:17:32 -0400

    Added outline for Bogey Logic

--- dotnet_objects/Game/Enemies/Bogey.cs
+++ dotnet_objects/Game/Enemies/Bogey.cs
@@ -1,18 +1,62 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Godot;
 
 namespace Game.Enemies
 {
     public partial class Bogey : Enemy
     {
+        private GameData gameData;
+
+        private Godot.Timer bogeyTimer;
+
+        private bool reposition;
+
+        private Vector2 targetPosition;
+
         public override void _Ready()
         {
             base._Ready();
             // Create a timer that ticks down every x seconds
             // when timer ends, the bogey moves to the player's x position and shooots
             // This will be done with a godot timer node.
+            reposition = true;
+            gameData = GetNode<GameData>("%GameData");
+            bogeyTimer = GetNode<Godot.Timer>("./BogeyTimer");
+            bogeyTimer.WaitTime = 2f;
+            bogeyTimer.Start();
+        }
+
+        public override void _Process(double delta)
+        {
+            if (reposition)
+            {
+                Position += Transform.X * 100 * (float)delta;
+                if (Position.X == targetPosition.X)
+                {
+                    reposition = false;
+                    GD.Print("Bogey reached target position, shooting");
+                    // Shoot
+                }
+                else
+                {
+                    float direction = targetPosition.X > Position.X ? 1 : -1;
+                    Position += Transform.X * 100 * (float)delta * direction;
+                }
+            }
+        }
+
+        public void OnBogeyTimerTimeout()
+        {
+            if (!reposition)
+            {
+                GD.Print("Bogey timer timeout");
+                targetPosition = gameData.GetPlayerX();
+                reposition = true;
+            }
         }
     }
 }
