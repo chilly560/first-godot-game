b4c094b755bdd33782083bb3d2171b9d03f783e5
toise560 <josephpcanale77@gmail.com>
2025-11-23 13:48:10 -0500

    Added signal for collision to HealthDrop

--- dotnet_objects/Game/Drops/ShotgunDrop.cs
+++ dotnet_objects/Game/Drops/ShotgunDrop.cs
@@ -1,12 +1,8 @@
 using Game.Drops;
-using Game.Enemies;
 using Game.Weapons;
-using Godot;
-using System;
 
 public partial class ShotgunDrop : Drop
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		collectable = WeaponFactory.CreateWeapon(
